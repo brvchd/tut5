b0fa9081deedaff660fd252e0f7a2d04cb4de09f
Dmytro Borovych <s18963@pjwstk.edu.pl>
2020-04-11 19:41:40 +0200

    Just a part of task

--- Controllers/EnrollmentController.cs
+++ Controllers/EnrollmentController.cs
@@ -1,20 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using tut3.DAL;
+using tut3.DTOs.Requests;
 using tut3.Models;
 
 namespace tut3.Controllers
 {
     [Route("api/enrollment")]
     [ApiController]
-    public class EnrollmentController : Controller
+    public class EnrollmentController : ControllerBase
     {
-        [HttpPost]
-        public IActionResult AddStudent(Student student)
+        private readonly IDbService _dbService;
+
+        public EnrollmentController(IDbService dbService)
         {
-            return Ok(student);
+            _dbService = dbService;
+        }
+
+        [HttpPost]
+        public IActionResult EnrollStudent(EnrollStudentRequest request)
+        {
+            using (var con = new SqlConnection("Data Source=db-mssql;Initial Catalog=s18963;Integrated Security=True"))
+            {
+                using (var com = new SqlCommand())
+                {
+                    com.Connection = con;
+                    com.CommandText = "Select * From Studies Where Name = @Name";
+                    com.Parameters.AddWithValue("Name", request.Studies);
+                    con.Open();
+
+                    var trans = con.BeginTransaction();
+                    com.Transaction = trans;
+                    var dr = com.ExecuteReader();
+
+                    if (!dr.Read())
+                    {
+                        dr.Close();
+                        trans.Rollback();
+                        return BadRequest("Specified studies does not exist");
+                    }
+
+                    int idStudy = (int)dr["IdStudy"];
+
+                }
+            }
+            return Ok();
         }
     }
 }
---
+++ DTOs/Requests/EnrollStudentRequest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tut3.DTOs.Requests
+{
+    public class EnrollStudentRequest
+    {
+        [Required]
+        [MaxLength(100)]
+        [RegularExpression("^s[0-9]+$")]
+        public string IndexNumber { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; }
+        [Required]
+        public string BirthDate { get; set; }
+        [Required]
+        public string Studies { get; set; }
+    }
+}
